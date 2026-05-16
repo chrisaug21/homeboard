7eeeba06f520250f5f6e66ec6a7a0805bd1712f0
Chris Augustine <cg.augustine@gmail.com>
2026-05-16 00:36:44 -0400

    Add room for marketing todo preview

--- js/shared.js
+++ js/shared.js
@@ -36,7 +36,7 @@
       return sb || initSupabaseClient();
     }
 
-    const VERSION = "2.0.39";
+    const VERSION = "2.0.40";
     const rotationIntervalMs = 30000;
     const marketingApp = document.getElementById("marketing-app");
     const displayApp = document.getElementById("display-app");
